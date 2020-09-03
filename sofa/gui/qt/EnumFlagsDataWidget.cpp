e25a52537b1e36928a709bd5d65f2e9bfeab1835
Gauthier Zirnhelt <gauthier.zirnhelt@insimo.fr>
2020-09-03 19:05:09 +0200

    [ISSofaGUI] Add EnumFlagsDataWidget (//Ref// T9937)
    This widget is responsible for displaying a grid of all possible flags
    of an enum. It detects the content of the enum through the
    AbstractTypeInfo API.
    
    You can opt into using it by setting the widget of a BaseData to
    "enum_flags".
    
    (cherry picked from commit 12b397b517d8f68aa6f1a8bb114c94682ebfba31)

---
+++ sofa/gui/qt/EnumFlagsDataWidget.cpp
@@ -0,0 +1,179 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  *
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program; if not, write to the Free Software Foundation, Inc., 51  *
+* Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.                   *
+*******************************************************************************
+*                            SOFA :: Applications                             *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include "EnumFlagsDataWidget.h"
+
+#include <QGridLayout>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <cassert>
+
+namespace sofa
+{
+namespace gui
+{
+namespace qt
+{
+
+#define MAX_COLS 5
+
+helper::Creator<DataWidgetFactory, EnumFlagsDataWidget> DWClass_EnumFlags("enum_flags", false);
+
+bool EnumFlagsDataWidget::createWidgets()
+{
+    const sofa::defaulttype::AbstractEnumTypeInfo* enumTypeInfo = getAbstractEnumTypeInfo();
+    if (!enumTypeInfo)
+    {
+        std::cerr << "EnumFlagsDataWidget: no AbstractEnumTypeInfo for BaseData " << this->getBaseData()->getName() << " of type " << this->getBaseData()->getValueTypeString() << std::endl;
+        return false;
+    }
+
+    // find the number of items to have an idea of how many flags there is
+    std::vector<std::string> items;
+    enumTypeInfo->getAvailableItems(this->getBaseData(), items);
+    std::reverse(items.begin(), items.end());
+
+    // find all possible flags
+    auto e = enumTypeInfo->createInstance();
+    for (std::size_t i = 0u; i < items.size(); ++i)
+    {
+        enumTypeInfo->setDataValueInteger(e.get(), 1 << i);
+        if (enumTypeInfo->getDataValueInteger(e.get()) != 1 << i) continue; // not a valid enumeration value
+        const std::string s = enumTypeInfo->getDataEnumeratorString(e.get());
+
+        _checkBoxes.emplace_back(new QCheckBox(this));
+    }
+    _checkBoxesLastValue.resize(_checkBoxes.size());
+
+    // create widgets corresponding to each flag
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    QGridLayout* grid = new QGridLayout(2 * std::ceil(static_cast<double>(_checkBoxes.size()) / MAX_COLS), std::min(static_cast<int>(_checkBoxes.size()), MAX_COLS));
+
+    for (std::size_t i = 0u; i < _checkBoxes.size(); ++i)
+    {
+        enumTypeInfo->setDataValueInteger(e.get(), 1 << i);
+        const std::string s = enumTypeInfo->getDataEnumeratorString(e.get());
+        grid->addWidget(new QLabel(QString::fromStdString(s), this), 2 * (i / MAX_COLS), i % MAX_COLS, Qt::AlignHCenter);
+        grid->addWidget(_checkBoxes[i], 1 + 2 * (i / MAX_COLS), i % MAX_COLS, Qt::AlignHCenter);
+
+        connect(_checkBoxes[i], SIGNAL(toggled(bool)), this, SLOT(setWidgetDirty()));
+    }
+
+    layout->addLayout(grid);
+
+    // add buttons to check or uncheck all checkboxes
+    QHBoxLayout* hlayout = new QHBoxLayout();
+    QPushButton* checkBtn1 = new QPushButton("Check all", this);
+    hlayout->add(checkBtn1);
+    connect(checkBtn1, SIGNAL(clicked()), this, SLOT(checkAll()));
+    QPushButton* checkBtn2 = new QPushButton("Uncheck all", this);
+    hlayout->add(checkBtn2);
+    connect(checkBtn2, SIGNAL(clicked()), this, SLOT(uncheckAll()));
+
+    layout->addLayout(hlayout);
+
+    readFromData();
+
+    return true;
+}
+
+const sofa::defaulttype::AbstractEnumTypeInfo* EnumFlagsDataWidget::getAbstractEnumTypeInfo() const
+{
+    const sofa::defaulttype::AbstractTypeInfo* typeInfo = this->getBaseData()->getValueTypeInfo();
+    assert(typeInfo->IsEnum());
+    return typeInfo->EnumType();
+}
+
+void EnumFlagsDataWidget::setDataReadOnly(bool readOnly)
+{
+    for (QCheckBox* cb : _checkBoxes)
+    {
+        cb->setEnabled(!readOnly);
+    }
+}
+
+bool EnumFlagsDataWidget::checkDirty()
+{
+    return true;
+}
+
+void EnumFlagsDataWidget::readFromData()
+{
+    const sofa::defaulttype::AbstractEnumTypeInfo* enumTypeInfo = getAbstractEnumTypeInfo();
+    const long long value = enumTypeInfo->getDataValueInteger(this->getBaseData()->getValueVoidPtr());
+
+    for (std::size_t i = 0u; i < _checkBoxes.size(); ++i)
+    {
+        const bool flag = value & (1 << i);
+
+        _checkBoxesLastValue[i] = flag;
+        _checkBoxes[i]->setChecked(flag);
+    }
+}
+
+void EnumFlagsDataWidget::writeToData()
+{
+    const sofa::defaulttype::AbstractEnumTypeInfo* enumTypeInfo = getAbstractEnumTypeInfo();
+    long long value = 0;
+
+    for (std::size_t i = 0u; i < _checkBoxes.size(); ++i)
+    {
+        const bool flag = _checkBoxes[i]->isChecked();
+        _checkBoxesLastValue[i] = flag;
+        if (flag)
+        {
+            value |= (1 << i);
+        }
+    }
+
+    // the following does not work for enum flags as the given value does not exactly equal an enum value
+    //enumTypeInfo->setDataValueInteger(this->getBaseData()->beginEditVoidPtr(), value);
+    //this->getBaseData()->endEditVoidPtr();
+
+    // directly write the underlying type of the enum to circumvent this issue
+    const sofa::defaulttype::AbstractTypeInfo* valueTypeInfo = enumTypeInfo->getFinalValueType();
+    assert(valueTypeInfo->IsSingleValue());
+    valueTypeInfo->SingleValueType()->setFinalValueInteger(this->getBaseData()->beginEditVoidPtr(), 0, value);
+    this->getBaseData()->endEditVoidPtr();
+}
+
+void EnumFlagsDataWidget::checkAll()
+{
+    for (QCheckBox* cb : _checkBoxes)
+    {
+        cb->setChecked(true);
+    }
+}
+
+void EnumFlagsDataWidget::uncheckAll()
+{
+    for (QCheckBox* cb : _checkBoxes)
+    {
+        cb->setChecked(false);
+    }
+}
+
+}
+}
+}
---
+++ sofa/gui/qt/EnumFlagsDataWidget.h
@@ -0,0 +1,80 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, version 1.0 RC 1        *
+*                (c) 2006-2011 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  *
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program; if not, write to the Free Software Foundation, Inc., 51  *
+* Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.                   *
+*******************************************************************************
+*                            SOFA :: Applications                             *
+*                                                                             *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#ifndef MATERIAL_DATAWIDGET_H
+#define MATERIAL_DATAWIDGET_H
+#include "DataWidget.h"
+#include <sofa/helper/vector.h>
+
+#include <QCheckBox>
+
+namespace sofa
+{
+namespace gui
+{
+namespace qt
+{
+
+class EnumFlagsDataWidget : public DataWidget
+{
+    Q_OBJECT
+public:
+    EnumFlagsDataWidget(QWidget* parent,
+                        const char* name,
+                        core::objectmodel::BaseData* data):
+        DataWidget(parent, name, data)
+    {}
+
+    template<class T>
+    static T* create(T* instance, const CreatorArgument& arg)
+    {
+        if (std::strcmp(arg.data->getWidget(), "enum_flags")) return nullptr; // "enum_flags" widget name required since this widget does not use a specific type of Data
+        return DataWidget::create(instance, arg);
+    }
+
+    virtual bool createWidgets() override;
+    virtual unsigned int numColumnWidget() override { return _checkBoxes.size(); }
+
+protected:
+    const sofa::defaulttype::AbstractEnumTypeInfo* getAbstractEnumTypeInfo() const;
+
+    virtual void setDataReadOnly(bool readOnly) override;
+    virtual bool checkDirty() override;
+    virtual void readFromData() override;
+    virtual void writeToData() override;
+
+    helper::vector<QCheckBox*> _checkBoxes;
+    helper::vector<bool>       _checkBoxesLastValue;
+
+public Q_SLOTS:
+    void checkAll();
+    void uncheckAll();
+};
+
+}
+}
+}
+
+#endif
+
